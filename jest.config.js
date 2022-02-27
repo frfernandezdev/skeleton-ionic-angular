ee3d86174bd4ba1e5b59905ca472b8eabf3f5f83
Fernando Fernandez <frfernandezdev@gmail.com>
2022-02-27 18:03:37 -0400

    build(jest): added test ignore files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-  },
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: {},
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: ["<rootDir>/cypress/"],
 };
