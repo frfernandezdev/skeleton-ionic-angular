e4381f4e0f3867899db17eed5ce5e9ac0aa52a86
Fernando Fernandez <frfernandezdev@gmail.com>
2022-03-04 16:16:11 -0400

    feat(app/shared): added NumberValueObject

---
+++ src/app/shared/domain/value-object/NumberValueObject.ts
@@ -0,0 +1,19 @@
+export abstract class NumberValueObject {
+	public readonly value: number;
+
+	public constructor(value: number) {
+		this.value = value;
+	}
+
+	public equalsTo(other: NumberValueObject): boolean {
+		return this.value === other.value;
+	}
+
+	public isBiggerThan(other: NumberValueObject): boolean {
+		return this.value > other.value;
+	}
+
+	public isLessThan(other: NumberValueObject): boolean {
+		return this.value < other.value;
+	}
+}
