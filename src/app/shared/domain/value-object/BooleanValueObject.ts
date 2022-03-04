47ed61f8f4b886885437448126f88cf92d8abd4e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-03-04 16:14:21 -0400

    feat(app/shared): added BooleanValueObject

---
+++ src/app/shared/domain/value-object/BooleanValueObject.ts
@@ -0,0 +1,15 @@
+export abstract class BooleanValueObject {
+	public readonly value: boolean;
+
+	public constructor(value: boolean) {
+		this.value = value;
+	}
+
+	public isTrue(): boolean {
+		return this.value === true;
+	}
+
+	public isFalse(): boolean {
+		return this.value === false;
+	}
+}
