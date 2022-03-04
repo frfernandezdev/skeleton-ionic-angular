e6d2a10d89f3d5894adf5318430d312162bbc590
Fernando Fernandez <frfernandezdev@gmail.com>
2022-03-04 15:29:11 -0400

    refactor(app): delete app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/CoreModule';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [BrowserModule, IonicModule.forRoot(), CoreModule],
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
