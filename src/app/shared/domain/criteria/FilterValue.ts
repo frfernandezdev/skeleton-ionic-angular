002435157778d9ee432ba6651fd3deaae29d5f5f
Fernando Fernandez <frfernandezdev@gmail.com>
2022-03-05 07:31:24 -0400

    feat(app/shared): added fromArray functionality

--- src/app/shared/domain/criteria/FilterValue.ts
+++ src/app/shared/domain/criteria/FilterValue.ts
@@ -4,4 +4,8 @@ export class FilterValue extends StringValueObject {
   public constructor(value: string) {
     super(value);
   }
+
+  public static fromArray(value: string[]): string {
+    return value.join(',').toString();
+  }
 }
