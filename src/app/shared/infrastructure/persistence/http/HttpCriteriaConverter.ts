8f98953629cb78afb63651bd0757d4d20e8afdc3
Fernando Fernandez <frfernandezdev@gmail.com>
2022-03-09 13:32:08 -0400

    feat(app/shared): added HttpCriteriaConverter layer

--- src/app/shared/infrastructure/persistence/http/HttpCriteriaConverter.ts
+++ src/app/shared/infrastructure/persistence/http/HttpCriteriaConverter.ts
@@ -1,9 +1,11 @@
-import type { Operator } from 'src/app/shared/domain/criteria/FilterOperator';
 import type { Filter } from 'src/app/shared/domain/criteria/Filter';
+import { Operator } from 'src/app/shared/domain/criteria/FilterOperator';
 import { HttpParams } from '@angular/common/http';
-import { Query, Type } from '@angular/core';
+import { Criteria } from 'src/app/shared/domain/criteria/Criteria';
+import { Filters } from 'src/app/shared/domain/criteria/Filters';
 
 export enum TypeQueryEnum {
+  EQUAL = '=',
   NOT_EQUAL = '!',
   GT = 'gt',
   LT = 'lt',
@@ -38,25 +40,84 @@ export class HttpCriteriaConverter {
     this.queryTransformers = new Map<
       Operator,
       TransformerFunction<Filter, QueryObject>
-    >({});
+    >([
+      [Operator.EQUAL, this.equalQuery],
+      [Operator.NOT_EQUAL, this.notEqualQuery],
+      [Operator.GT, this.greaterThatQuery],
+      [Operator.LT, this.lessThanQuery],
+      [Operator.GOET, this.greaterOrEqualThanQuery],
+      [Operator.LOET, this.lessOrEqualThanQuery],
+      [Operator.CONTAINS, this.containsThanQuery],
+      [Operator.NOT_CONTAINS, this.notContainsThatQuery],
+      [Operator.LIKE, this.likeThanQuery],
+      [Operator.NOT_LIKE, this.noLikeThanQuery],
+      [Operator.ILIKE, this.ilkeThanQuery],
+      [Operator.NOT_ILIKE, this.noILikeThanQuery],
+      [Operator.BETWEEN, this.betweenThanQuery],
+      [Operator.NOT_BETWEEN, this.notBetweenThanQuery],
+    ]);
   }
 
-  private parseValueToString(operator, value) {
+  public convert(criteria: Criteria): HttpParams {
+    const { filters, order, offset, limit } = criteria;
+    const params = new HttpParams();
+
+    if (criteria.hasOrder()) {
+      const { orderBy, orderType } = order;
+      params.set('orderBy', orderBy.value);
+      params.set('orderType', orderType.value);
+    }
+
+    if (offset) {
+      params.set('offset', offset);
+    }
+
+    params.set('limit', limit);
+    params.set('offset', offset);
+
+    if (criteria.hasFilters()) {
+      this.generateQuery(params, filters);
+    }
+
+    return params;
+  }
+
+  protected generateQuery(params: HttpParams, filters: Filters): void {
+    filters.filters.map((filter) => {
+      const { field, value } = this.queryForFilter(filter);
+
+      params.set(field, value);
+    });
+  }
+
+  private queryForFilter(filter: Filter): QueryObject {
+    const functionToApply = this.queryTransformers.get(filter.operator.value);
+
+    if (!functionToApply) {
+      throw Error(`Unexpected operator value ${filter.operator.value}`);
+    }
+
+    return functionToApply(filter);
+  }
+
+  private parseValueToString(operator: string, value: string): string {
     return [operator, value].join(':');
   }
 
-	private parseValueToRange(operator, value) {
-		return [operator, []]
-	}
-
   private equalQuery(filter: Filter): QueryObject {
     const { field, value } = filter;
-    return { field: field.value, value: value.value };
+    return {
+      field: field.value,
+      value: value.value,
+    };
   }
 
   private notEqualQuery(filter: Filter): QueryObject {
     const { field, value } = filter;
-    return { field: field.value, value: `!${value.value}` };
+    return {
+      field: field.value,
+      value: `!${value.value}`,
+    };
   }
 
   private greaterThatQuery(filter: Filter): QueryObject {
